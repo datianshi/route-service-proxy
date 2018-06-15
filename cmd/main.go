7ed2bff9b7ce43d582034631558f16a5ba88b789
Shaozhen Ding <dsz0111@gmail.com>
2018-06-15 15:48:49 -0500

    get a working around to close idle connections so the http requests can honor the dns change

--- cmd/main.go
+++ cmd/main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"crypto/tls"
 	"net/http"
+	"time"
 
 	"github.com/datianshi/route-service-proxy/routing"
 )
@@ -15,6 +16,15 @@ func main() {
 	url := func(a string) string {
 		return a
 	}
+
+	//This is to accormodate the issue DNS can not updated duto idle connections
+	//https://github.com/golang/go/issues/23427
+	go func() {
+		for {
+			transport.CloseIdleConnections()
+			time.Sleep(5 * time.Second)
+		}
+	}()
 	server := &http.Server{
 		Addr:    ":8080",
 		Handler: routing.NewProxy(transport, url),
